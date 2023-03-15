16884087fb21d12b64d22552fa47f37a96e67e91
Nolobolo28 <gnolan475@gmail.com>
2023-03-14 21:14:26 -0400

    added settings menu

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // eventListener on setting icon, question mark to give up and enter image
 const form = $("#my-form");
 const addDiv = $(".guess-location-div");
+const settings = $(".settings");
 let answer = {};
 let colors = [];
 let guesses = 0;
@@ -11,13 +12,13 @@ let haversineDistance;
 
 (async function getCountries() {
   //const res = await fetch(
-  "https://restcountries.com/v3.1/all?fields=name,latlng,population,independent,region"
-  );
+    //"https://restcountries.com/v3.1/all?fields=name,latlng,population,independent,region"
+  //);
   const countryData = await res.json();
   let randoNum = Math.floor(Math.random() * 251);
   while (countryData[randoNum].independent !== true) {
     randoNum = Math.floor(Math.random() * 251);
-    console.log("ran");
+    //console.log("ran");
   }
   const hemi = countryData[randoNum].latlng[0] > 0 ? "Northern" : "Southern";
   answer = {
@@ -48,10 +49,12 @@ function getGuess(ev) {
         population: countries[i].population,
         hemisphere: countries[i].latlng[0] > 0 ? "Northern" : "Southern",
       };
+      console.log(answer);
+      createGuess();
+    } else {
+      form.trigger("reset");
     }
   }
-  console.log(answer);
-  createGuess();
 }
 
 function createGuess() {
@@ -68,11 +71,11 @@ function createGuess() {
   let distance = Math.round(r * c);
   haversineDistance = distance; //this is how many miles the user guess is in relation to the answer
   console.log(haversineDistance);
-  for(let key in answer){
-    if(answer[key] === userGuess[key]){
-      colors.push("green")
-    }else {
-      colors.push("red")
+  for (let key in answer) {
+    if (answer[key] === userGuess[key]) {
+      colors.push("green");
+    } else {
+      colors.push("red");
     }
   }
   guesses++; // incrementing guesses so we can add it to the h5 dynamically
@@ -84,17 +87,44 @@ function createGuess() {
   );
   const hemisphere = colors[5];
   addDiv.append(
-    $(`<div>${userGuess.hemisphere}</div>`).addClass(`guess-hint-div ${hemisphere}`)
+    $(`<div>${userGuess.hemisphere}</div>`).addClass(
+      `guess-hint-div ${hemisphere}`
+    )
   );
-  const region = colors[1]
-  addDiv.append($(`<div>${userGuess.region}</div>`).addClass(`guess-hint-div ${region}`));
+  const region = colors[1];
+  addDiv.append(
+    $(`<div>${userGuess.region}</div>`).addClass(`guess-hint-div ${region}`)
+  );
   const popColor = colors[4];
   addDiv.append(
-    $(`<div>${userGuess.population}</div>`).addClass(`guess-hint-div ${popColor}`)
+    $(`<div>${userGuess.population}</div>`).addClass(
+      `guess-hint-div ${popColor}`
+    )
   );
   addDiv.append(
     $(`<div>${haversineDistance} miles</div>`).addClass("guess-hint-div blue")
   );
+  clear();
+}
+
+function showSettings() {
+  $(".hint-div").css("display", "none");
+  addDiv.css("display", "none");
+  $(".input-div").css("display", "none");
+}
+
+function clear() {
+  if (userGuess.name !== answer.name) {
+    userGuess = {};
+    colors = [];
+    haversineDistance;
+    guess = "";
+    form.trigger("reset");
+  } else {
+    setTimeout(() => location.reload(), 5000);
+  }
 }
 
 form.on("submit", getGuess);
+
+settings.on("click", showSettings);
